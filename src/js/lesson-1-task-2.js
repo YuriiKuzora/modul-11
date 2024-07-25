25af4b46a66c45c8099b669f584e6be0bfc187a5
YuriiKuzora <yurakuzora@gmail.com>
2024-07-25 17:45:49 +0200

    lesson 1 task 2

--- src/js/lesson-1-task-2.js
+++ src/js/lesson-1-task-2.js
@@ -1,4 +1,22 @@
 'use strict';
 
 // https://pixabay.com/api/
-// 45112912-6770c0bfd04bfeab336421b9c
+
+function getPicturesByQuery(query) {
+  const API_KEY = '45112912-6770c0bfd04bfeab336421b9c';
+
+  return fetch(
+    `https://pixabay.com/api?key=${API_KEY}&q=${query}`
+  ).then(res => {
+    console.log(res);
+
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
+
+getPicturesByQuery('blue+car')
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
