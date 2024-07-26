3580ca13dcc33642cdd98a984c347143b7c60146
YuriiKuzora <109381966+YuriiKuzora@users.noreply.github.com>
2024-07-26 18:30:57 +0000

    Deploying to gh-pages from @ YuriiKuzora/modul-11@1305f760586e069cf2e3618e077d50f455a0ef7e 🚀

--- commonHelpers8.js
+++ commonHelpers8.js
@@ -1,2 +1,16 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";document.querySelector(".card-container");document.querySelector(".search-form");
+import"./assets/modulepreload-polyfill-3cfb730f.js";function i(t){return fetch(`https://pokeapi.co/api/v2/pokemon/${t}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}const l=document.querySelector(".card-container"),d=document.querySelector(".search-form");d.addEventListener("submit",u);function u(t){t.preventDefault();const e=t.currentTarget,r=e.elements.query.value.toLowerCase();i(r).then(p).catch(m).finally(()=>e.reset())}function p({name:t,sprites:e,weight:r,height:c,abilities:o}){const a=o.map(({ability:s})=>`<li class="list-group-item">${s.name}</li>`).join(""),n=`
+      <div class="card">
+        <div class="card-img-top">
+          <img src="${e.front_default}" alt="${t}" />
+        </div>
+        <div class="card-body">
+          <h2 class="card-title">Ім'я: ${t}</h2>
+          <p class="card-text">Вага: ${r}</p>
+          <p class="card-text">Зростання: ${c}</p>
+          <p class="card-text"><b>Уміння</b></p>
+          <ul class="list-group">
+            ${a}
+          </ul>
+        </div>
+      </div>`;l.innerHTML=n}function m(t){alert("Упс, щось пішло не так і ми не знайшли вашого покемона!")}
 //# sourceMappingURL=commonHelpers8.js.map
